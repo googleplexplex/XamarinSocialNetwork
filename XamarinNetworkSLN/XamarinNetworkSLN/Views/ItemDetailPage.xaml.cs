9d69cbcda1d1b89e52349ab20ca0bf8c12465bde
Volodin Ivan <53963106+googleplexplex@users.noreply.github.com>
2024-02-06 00:29:39 +0300

    Добавлен основной костяк и шаблон проекта

---
+++ XamarinNetworkSLN/XamarinNetworkSLN/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using XamarinNetworkSLN.ViewModels;
+
+namespace XamarinNetworkSLN.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ XamarinNetworkSLN/XamarinNetworkSLN/Views/NewItemPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using XamarinNetworkSLN.Models;
+using XamarinNetworkSLN.ViewModels;
+
+namespace XamarinNetworkSLN.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
